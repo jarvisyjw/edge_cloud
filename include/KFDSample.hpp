76aad88892c7eda72da7f6189987ea133bf36d0c
11710414 <11710414@mail.sustech.edu.cn>
2022-08-12 16:04:51 +0800

    1.1

--- include/KFDSample.hpp
+++ include/KFDSample.hpp
@@ -1,7 +1,24 @@
+#include "ORBextractor.h"
+#include <opencv2/core.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+
+using namespace cv;
+using namespace std;
+
 class KFDSample
 {
 private:
-    /* data */
+    // ORBextractor
+    ORB_SLAM2::ORBextractor *mpORBextractor;
+    // ORBextractor Parameters
+    float scaleFactor = 1.2;
+    int nfeatures = 2000, nlevels = 8, iniThFAST = 20, minThFAST = 7;
+    // Tracking points
+    vector<Point2f> old, next;
+
+    
+
 public:
     KFDSample(/* args */);
     ~KFDSample();
@@ -11,6 +28,6 @@ KFDSample::KFDSample(/* args */)
 {
 }
 
-KFDSample::~KFDSample()
-{
-}
+// KFDSample::~KFDSample()
+// {
+// }
