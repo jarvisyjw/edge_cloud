ae60d12d93990441597a3678f90c8c4de0cacc14
11710414 <11710414@mail.sustech.edu.cn>
2022-08-13 10:19:17 +0800

    1.1.1

--- include/KFDSample.hpp
+++ include/KFDSample.hpp
@@ -1,4 +1,5 @@
 #include "ORBextractor.h"
+#include "pd.hpp"
 #include <opencv2/core.hpp>
 #include <opencv2/highgui.hpp>
 #include <opencv2/imgproc.hpp>
@@ -9,14 +10,22 @@ using namespace std;
 class KFDSample
 {
 private:
+    // Tracking points
+    vector<Point2f> old, next;
     // ORBextractor
     ORB_SLAM2::ORBextractor *mpORBextractor;
     // ORBextractor Parameters
     float scaleFactor = 1.2;
     int nfeatures = 2000, nlevels = 8, iniThFAST = 20, minThFAST = 7;
-    // Tracking points
-    vector<Point2f> old, next;
-
+    // Tracking Keypoints and Descriptors
+    vector<KeyPoint> mvKeys;
+    Mat mDescriptors;
+    // Image
+    Mat frame1, imprvs;
+    Mat frame2, imnext;
+    // PD controller
+    PD *mpPDcontroller;
+    
     
 
 public:
